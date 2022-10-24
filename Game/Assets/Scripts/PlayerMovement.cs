c156e3b3d9b79664c6872b540bd1b4dad34322d6
BorysMandryk <55620815+BorysMandryk@users.noreply.github.com>
2022-10-24 21:43:01 +0300

    Added jump

--- Game/Assets/Scripts/PlayerMovement.cs
+++ Game/Assets/Scripts/PlayerMovement.cs
@@ -5,19 +5,41 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] private float _speed = 10f;
+    [SerializeField] private float _movementSpeed = 10f;
+    [SerializeField] private float _jumpSpeed = 10f;
 
-    private Vector2 movementVec;
+    private Rigidbody2D _rigidbody;
+    private Collider2D _feetCollider;
+
+    private Vector2 _movementVec;
+
+    private void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+        _feetCollider = GetComponentInChildren<Collider2D>();
+    }
 
     private void OnMove(InputValue value)
     {
-        movementVec = value.Get<Vector2>();
+        _movementVec = value.Get<Vector2>();
     }
 
     private void HandleMove()
     {
-        
-        transform.Translate(new Vector3(movementVec.x * _speed * Time.deltaTime, 0f, 0f));
+        transform.Translate(new Vector3(_movementVec.x * _movementSpeed * Time.deltaTime, 0f, 0f));
+    }
+
+    private void OnJump(InputValue value)
+    {
+        Debug.Log("jump");
+        if (!_feetCollider.IsTouchingLayers(LayerMask.GetMask("Ground")))
+        {
+            return;
+        }
+        if (value.isPressed)
+        {
+            _rigidbody.velocity += new Vector2(0f, _jumpSpeed);
+        }
     }
 
     void Update()
